d843d702f5b48a831e4d6cf90162236eda33a180
George Brighton <george@gebn.co.uk>
2018-10-28 17:18:55 +0000

    Improve import formatting

--- commute_weather/darksky.py
+++ commute_weather/darksky.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
 import datetime
-
 import pytz
 import requests
 
--- commute_weather/util.py
+++ commute_weather/util.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
+from typing import Callable
 import logging
 import time
 import datetime
-from typing import Callable
-
 import requests
 
 logger = logging.getLogger(__name__)
